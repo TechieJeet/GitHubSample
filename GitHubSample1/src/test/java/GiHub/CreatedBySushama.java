5df725d74c94c0567165015d703cf37d1606c3e5
sushma <sushma@DESKTOP-T6A568G>
2025-11-04 10:02:42 +0530

    Code Done successfully

--- GitHubSample1/src/test/java/GiHub/CreatedBySushama.java
+++ GitHubSample1/src/test/java/GiHub/CreatedBySushama.java
@@ -2,10 +2,11 @@ package GiHub;
 
 public class CreatedBySushama {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Created");
-		System.out.println("Code Updated");
+		System.out.println("Code updated");
 	}
 
 }
